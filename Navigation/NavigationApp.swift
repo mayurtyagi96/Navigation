4a24ef1171a7459b48a39fe0ad91c2c0e4404ff5
Mayur <mayurtyagi@unolo.com>
2024-08-01 18:44:48 +0530

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Mayur  on 01/08/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
